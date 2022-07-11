7bd3f96bcb6356361754e3905dc789df3574accb
kelvin <kelvinperrie@gmail.com>
2022-07-11 15:58:40 +1200

    whew boy, just adding all this here garbage

--- src/index.js
+++ src/index.js
@@ -18,6 +18,30 @@ function Square(props) {
   );
 }
 
+class Tile extends React.Component {
+
+  render() {
+    return <div className="city-tile">aaaa</div>
+  }
+}
+
+class City extends React.Component {
+
+  render() {
+    let cityTiles = this.props.cityTiles;
+    const tilesHtml = cityTiles.map((item, index) => {
+
+      return (
+        <div key={index} className="city-tile" onClick={() => this.props.handleClickCityTile(index)}>
+          Tile {index} is {item!==null ? item : "?"}
+        </div>
+      );
+    });
+
+    return <div>{tilesHtml}</div>;
+  }
+}
+
 class Board extends React.Component {
 
   renderSquare(i) {
@@ -66,13 +90,172 @@ class Board extends React.Component {
 
 class Game extends React.Component {
   constructor(props) {
+    
+    const locations = [
+      {
+        name : "New Zealand",
+        arrivalCity : "Auckland",
+        clues : [ "They wanted to change their money to dollars", "Keen to visit middle earth", "Something about seeing friends in Wellington" ]
+      },
+      {
+        name : "Australia",
+        arrivalCity : "Sydney",
+        clues : [ "They wanted to change their money to dollars", "Keen to visit the Opera House", "Something about seeing friends in Perth" ]
+      },
+      {
+        name : "England",
+        arrivalCity : "London",
+        clues : [ "They wanted to change their money to pounds", "Keen to visit Big Ben", "Something about seeing friends in York" ]
+      },
+      {
+        name : "Scotland",
+        arrivalCity : "Edinburgh",
+        clues : [ "They wanted to change their money to pounds", "Keen to visit the Loch Ness monster", "Something about seeing friends in Glasgow" ]
+      },
+      {
+        name : "Egypt",
+        arrivalCity : "Cairo",
+        clues : [ "They wanted to change their money to pounds", "Keen to visit the Nile", "Something about seeing friends in Luxor" ]
+      },
+    ];
+
     super(props);
     this.state = {
       history : [ { squares: Array(9).fill(null), col: null, row: null} ],
       xIsNext : true,
       stepNumber : 0,
-      sortOrderAsc : true
+      sortOrderAsc : true,
+      locations : locations,
+      currentLocation : null,
+      lastSeenLocation : null,
+      nextLocation : null,
+      possibleChoices : [],
+      cityTiles : [ Array(9).fill(null) ],
+      clueToDisplay : null,
+      displayClue : false
     }
+
+    var currentLocation = this.getLocationByName("Scotland", locations);
+    var nextLocation = this.getNextLocation(currentLocation, locations);
+    var possibleChoices = this.getPossibleChoices(currentLocation, nextLocation, locations);
+    this.state.currentLocation = currentLocation;
+    this.state.lastSeenLocation = currentLocation;
+    this.state.nextLocation = nextLocation;
+    this.state.possibleChoices = possibleChoices;
+    this.state.cityTiles = this.createCityTiles(nextLocation);
+    // this.setState({ 
+    //   currentLocation : currentLocation,
+    //   nextLocation : nextLocation
+    // })
+  }
+
+  travelToLocation(travelToLocation) {
+    let locations = this.state.locations;
+    if(travelToLocation.name === this.state.nextLocation.name) {
+      // they are moving to the correct place :)
+      var currentLocation = travelToLocation;
+      var nextLocation = this.getNextLocation(currentLocation, locations);
+      var possibleChoices = this.getPossibleChoices(currentLocation, nextLocation, locations);
+      this.setState({
+        currentLocation : currentLocation,
+        lastSeenLocation : currentLocation,
+        nextLocation : nextLocation,
+        possibleChoices : possibleChoices,
+        cityTiles : this.createCityTiles(nextLocation)
+      })
+
+    } else {
+      // they are not moving to the correct place ...
+
+    }
+  }
+
+  createCityTiles(nextLocation) {
+    let cityTiles = Array(9).fill(null);
+    console.log("we have this many clues")
+    console.log(nextLocation.clues.length)
+    for(let i = 0; i < nextLocation.clues.length; i++) {
+      var clueSet = false;
+      while(!clueSet) {
+        var cityIndex = randomIntFromInterval(0, 8);
+        console.log(cityTiles[cityIndex])
+        if(null === cityTiles[cityIndex]) {
+          console.log("setting clue " + i + " to tile " + cityIndex)
+          cityTiles[cityIndex] = i;
+          clueSet = true;
+          
+        }
+      }
+    }
+    return cityTiles;
+  }
+
+  getLocationByName(name, locations) {
+    for(let i = 0; i < locations.length; i++) {
+      console.log(locations[i])
+      if(locations[i].name === name) {
+        return locations[i];
+      }
+    }
+  }
+
+  getNextLocation(currentLocation, locations) {
+    console.log("in getNextLocation")
+    console.log(locations)
+    var nextLocationIndex = randomIntFromInterval(0, locations.length-1);
+    var nextLocation = locations[nextLocationIndex];
+    if(currentLocation.name === nextLocation.name) {
+      nextLocation = this.getNextLocation(currentLocation, locations);
+    }
+    return nextLocation;
+  }
+
+  getPossibleChoices(currentLocation, nextLocation, locations) {
+    var possibleChoices = [];
+    while(possibleChoices.length < 2) {
+      var possibleChoice = null;
+      var nextChoiceIndex = randomIntFromInterval(0, locations.length-1);
+      possibleChoice = locations[nextChoiceIndex];
+      if(possibleChoice.name !== currentLocation.name && possibleChoice.name !== nextLocation.name) {
+        var duplicate = false;
+        for(let i = 0; i <possibleChoices.length; i++) {
+          if(possibleChoices[i].name === possibleChoice.name) {
+            duplicate = true;
+            break;
+          }
+        }
+        if(!duplicate) {
+          possibleChoices.push(possibleChoice);
+        }
+      }
+    }
+    var insertIndex = randomIntFromInterval(0, 2);
+    possibleChoices.splice(insertIndex,0,nextLocation);
+    return possibleChoices;
+  }
+
+  handleClickTravelTo(name) {
+    let travelToThisLocation = this.getLocationByName(name, this.state.locations);
+    this.travelToLocation(travelToThisLocation);
+  }
+
+  handleClickCityTile(index) {
+    var clickedCityTileData = this.state.cityTiles[index];
+    if(clickedCityTileData!==null) {
+      console.log("data is " + clickedCityTileData);
+      let clue = this.state.nextLocation.clues[clickedCityTileData];
+      console.log("clue is " + clue)
+      this.setState({
+        clueToDisplay : clue,
+        displayClue : true
+      })
+    } else {
+      this.setState({
+        clueToDisplay : "you look like a cop, I don't talk to cops",
+        displayClue : true
+      })
+    }
+    console.log("clicked on " + index)
   }
 
   jumpTo(step) {
@@ -114,20 +297,32 @@ class Game extends React.Component {
     const winner = winningInfo.winner;
     const winningSquares = winningInfo.winningSquares;
 
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move + " col:" + step.col + ", row:" + step.row :
-        'Go to game start';
-      let stepStyle = "";
-      if(move === this.state.stepNumber) {
-        stepStyle = "active-step";
-      }
+    // const moves = history.map((step, move) => {
+    //   const desc = move ?
+    //     'Go to move #' + move + " col:" + step.col + ", row:" + step.row :
+    //     'Go to game start';
+    //   let stepStyle = "";
+    //   if(move === this.state.stepNumber) {
+    //     stepStyle = "active-step";
+    //   }
+    //   return (
+    //     <li key={move}>
+    //       <button className={stepStyle} onClick={() => this.jumpTo(move)}>{desc}</button>
+    //     </li>
+    //   );
+    // });
+
+    var possibleChoices = this.state.possibleChoices;
+    const possibleChoicesHtml = possibleChoices.map((choice, index) => {
+
       return (
-        <li key={move}>
-          <button className={stepStyle} onClick={() => this.jumpTo(move)}>{desc}</button>
+        <li key={index}>
+          <button className="possible-choice" onClick={ () => this.handleClickTravelTo(choice.name) }>{choice.name}</button>
         </li>
       );
     });
+
+    var clueDisplay = <div className="clue-display">{this.state.clueToDisplay}</div>;
 
     let status;
     if (winner) {
@@ -140,16 +335,25 @@ class Game extends React.Component {
 
     return (
       <div className="game">
-        <div className="game-board">
-          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} winningSquares={winningSquares} />
+        <div className="location-info">
+          <div className="location">Current: { this.state.currentLocation ? this.state.currentLocation.name + "!" : "unknown" }</div>
+          <div className="location">Last Seen:    { this.state.lastSeenLocation ? this.state.lastSeenLocation.name + "!" : "unknown" }</div>
+          <div className="location">Next:    { this.state.nextLocation ? this.state.nextLocation.name + "!" : "unknown" }</div>
+          <div>Where do you want to go? {possibleChoicesHtml}</div>
         </div>
-        <div className="game-info">
+        <br/>
+        <div className="game-board">
+          {/* <Board squares={current.squares} onClick={(i) => this.handleClick(i)} winningSquares={winningSquares} /> */}
+          <City cityTiles={this.state.cityTiles} handleClickCityTile={(index) => this.handleClickCityTile(index)}/>
+        </div>
+          {this.state.displayClue ? clueDisplay : ""}
+        {/* <div className="game-info">
           <div>{ status }</div>
           <ol>{ this.state.sortOrderAsc ? moves : moves.reverse() }</ol>
           <button onClick={() => this.handleSwitcharooClick()}>
             Do A Switcharoo
           </button>
-        </div>
+        </div> */}
       </div>
     );
   }
@@ -187,4 +391,9 @@ function calculateWinner(squares) {
     }
   }
   return {};
+}
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
