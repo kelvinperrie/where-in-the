5ef7bab43c9049431c3db5d800314558c92131c1
kelvin <kelvinperrie@gmail.com>
2022-08-28 18:51:04 +1200

    madness, letting them close a clue by clicking another city tile

--- src/index.js
+++ src/index.js
@@ -508,7 +508,9 @@ class Game extends React.Component {
     // don't allow if intro still open
     if(this.state.showIntroduction) return;
     // can't click a tile if the dialog is already open. We're not animals. We live in a society. There are rules.
+    // close the currently opened clue
     if(this.state.displayClue) {
+      handleClickCloseCloseDisplay(); // this function just has the best name. I can't beat this.
       return;
     }
     let clickedCityTileData = this.state.cityTiles[index];
